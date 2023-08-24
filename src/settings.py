dbd8208d4baecd98e170ff7d249724778fadd844
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-24 11:32:40 +0600

    PREVIEW_IMAGE_ID

--- src/settings.py
+++ src/settings.py
@@ -41,7 +41,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.mvtec.com/company/research/datasets/mvtec-loco"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 2048364
+PREVIEW_IMAGE_ID: int = 2049371
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/mvtec-loco-ad"
